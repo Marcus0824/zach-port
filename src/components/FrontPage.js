f7b41c941cd126ffbccc2570d331c072c04c1adc
Marcus0824 <31786365+Marcus0824@users.noreply.github.com>
2022-04-13 23:05:51 -0400

    Animation Testing
    Added framer and made front page title do a glow.

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,12 +1,30 @@
 
 import React from 'react'
+import { motion } from "framer-motion"
 import './style/FrontPage.scss'
+
+
 
 const FrontPage = () => {
   return (
-    <div className = "title">
-      ZACH HUSSEIN
-    </div>
+    <>
+      <div className = "page">
+        <motion.div 
+          animate={{opacity: [0, 1]}} 
+          whileHover={{color: ["rgb(0, 0, 0)","#ef4824"]}}
+        >
+          <div className="text">
+            زاك
+          </div>
+
+          <div classname="text">
+            ZACH HUSSEIN
+          </div>
+        </motion.div>
+      </div>
+
+      
+    </>
   )
 }
 
