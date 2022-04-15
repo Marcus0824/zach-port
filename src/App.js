7589fef965bf2ac1052e433a6a47188620a151ac
szammyboi <szammyboi@gmail.com>
2022-04-14 23:49:37 -0400

    Added Routing and Experimented With More Custom Animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.scss';
-import FrontPage from './components/FrontPage.js'
+import FrontPage from './components/FrontPage'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+/*
+  Set Up Basic Routing
+*/
 
 function App() {
   return (
         <div>
-          <FrontPage />
+          <Router>
+            <Routes>
+              <Route path="/" element={<FrontPage />} />
+              <Route path="/home" element={<div>
+                  <h1>PICTURES</h1>
+              </div>} />
+            </Routes>
+          </Router>
         </div>
 
   )
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,30 +1,45 @@
 
-import React from 'react'
-import { motion } from "framer-motion"
+import React, { useState, useEffect } from 'react'
+import { motion, useMotionValue } from "framer-motion"
 import './style/FrontPage.scss'
+import { Link } from "react-router-dom";
 
 
 
+/*
+  I changed the way the animation is handled just so that I could test trigger animations on objects
+  that aren't within the same motion.div
+
+  The Enter button animates with the Zach Hussein text even though they are separate
+  Just wanted to experiment
+
+*/
 const FrontPage = () => {
+  const [transitionColor, setTransitionColor] = useState(0);
+  const colors = ["#000000", "#ef4824"];
   return (
-    <>
-      <div className = "page">
+      <div className="page">
         <motion.div 
-          animate={{opacity: [0, 1]}} 
-          whileHover={{color: ["rgb(0, 0, 0)","#ef4824"]}}
+          animate={{opacity: [0, 1], color: colors[+transitionColor]}} 
+          onHoverStart={() => {setTransitionColor(!transitionColor)}}
+          onHoverEnd={() => {setTransitionColor(!transitionColor)}}
+          //whileHover={{color: ["#000000","#ef4824"]}}
         >
           <div className="text">
             زاك
           </div>
-
-          <div classname="text">
+          <div className="text">
             ZACH HUSSEIN
           </div>
         </motion.div>
-      </div>
 
-      
-    </>
+        <Link to="home" className="smalltext">
+          <motion.div className='bruh' animate={{opacity: [0, 1], color: colors[+(!transitionColor)]}}>
+            ENTER
+          </motion.div>
+        </Link>
+        
+      </div>
   )
 }
 
